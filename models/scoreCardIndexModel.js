d4228eee4304fa46b3eb62f7c397d124f720b84e
TaikiFnit <taikifnitspg@gmail.com>
2015-07-05 17:43:44 +0900

    bugfix: insertOwnScoreCard, scoreTotal insert 時のSQL文を修正

--- models/scoreCardIndexModel.js
+++ models/scoreCardIndexModel.js
@@ -160,13 +160,15 @@ function scoreCardIndexModel(io, connection, sessions) {
 
 								// 得点表作成
 								var insertScoreCardSql = 'insert into scoreCard(p_id, m_id, created, place) values(' + connection.escape(results[0].p_id) + ', ' + connection.escape(data.m_id) + ', now(), "ふにっと競技場")';
+								console.log(insertScoreCardSql);
 
 								connection.query(insertScoreCardSql, function (err, insertScoreCardData) {
 									console.log('insertScoreCard results');
 									console.log(insertScoreCardData);
 
 									// 得点表データに対応するscoreTotalのrecordをinsertする
-									var insertScoreTotalSql = 'insert into scoreTotal(sc_id, p_id, o_id) values(' + insertScoreCardData.insertId + ', ' + connection.escape(results[0].p_id) + ', ' + connection.escape(data.m_id) + ');';
+									var insertScoreTotalSql = 'insert into scoreTotal(sc_id, p_id, o_id) values(' + insertScoreCardData.insertId + ', ' + connection.escape(results[0].p_id) + ', ' + connection.escape(results[0].o_id) + ');';
+									console.log(insertScoreTotalSql);
 
 									connection.query(insertScoreTotalSql, function(err, insertScoreTotalData) {
 
