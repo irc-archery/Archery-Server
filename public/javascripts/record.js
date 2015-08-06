8d8d5d569781a7476dc3326b24c682b9323fd5bb
TaikiFnit <taikifnitspg@gmail.com>
2015-08-06 22:22:11 +0900

    implement record page

--- public/javascripts/record.js
+++ public/javascripts/record.js
@@ -1,12 +1,26 @@
+var match = {};
+
 $(function() {
 
 	$.ajax({
-		url: '/app/personal/' + getQueryString().sc_id,
+		url: '/app/personal/record/' + getQueryString().sc_id,
 		type: 'GET',
 		dataType: 'json',
 		success: function(data, textStatus) {
 
 			console.log(data);
+
+			if(data != '') {
+
+				// 試合データを保存
+				match = data;
+
+				// 得点表データを出力
+				viewScoreCard(data);	
+
+				// 修正用のhover effectを削除
+				$('.comp').removeClass('comp');
+			}
 
 		},
 		error: function(err) {
--- public/javascripts/scoreCard.js
+++ public/javascripts/scoreCard.js
@@ -30,41 +30,8 @@ socket.on('extractScoreCard', function(data) {
   number = data.number;
   prefectures = data.prefectures;
 
-  // 得点表の個人情報を出力
-  $('.matchNameTextBox').val(data.matchName);
-  $('.createdTextBox').val(data.created);
-  $('.numberTextBox').val(data.number);
-  $('.playerNameTextBox').val(data.playerName);
-  $('.organizationNameTextBox').val(data.organizationName);
-  $('.prefecturesTextBox').val(data.prefectures);
-
-  // subtotalの出力
-  for(var i = 0; i < 6; i++) {
-  	if(data.score[i] != undefined) {
-	  	$('.subTotalHeader' + (i + 1)).text(data.score[i].subTotal);
-  	}
-  }
-
-  // total, 10数, x数の出力
-  $('.subTotalHeaderTotal').text(data.total);
-  $('.subTotalHeaderTen').text(data.ten);  
-  $('.subTotalHeaderX').text(data.x);  
-
-  if(match.number == null) {
-  	match.number = '';
-  }
-
-  var lengthOption = ["90m", "70m", "60m", "50m", "40m", "30m", "70m前", "70m後"];
-
-  match.length = lengthOption[data.length];
-
-  for(var i = 0; i < data.countPerEnd; i++) {
-    // 得点の表示
-  	viewScore(data.score[i]);
-
-    // update対応
-    $('.perEnd' + data.score[i].perEnd + ' .score div').addClass('comp');
-  }
+  // 得点表データの出力
+  viewScoreCard(data);
 
   // 得点表編集モード
   if(data.permission == true) {
@@ -149,75 +116,6 @@ socket.on('broadcastUpdateScore', function (data) {
    // 計算したsubTotalを表示 it include processing();
    viewUpdate(data.perEnd, arrows, data['updatedScore_' + arrows]);;
 });
-
-// output scores
-function viewScore(score) {
-
-	var code = '';
-
-	code += '<div class="perEnd' + score.perEnd + '">';
-  code += '<div class="scoreAreaHeader rows">';
-	code += '<div class="col-fn-10 scoreRowsInfo"><span class="perEnd">' + score.perEnd + '</span>回目 : <span class="length">' + match.length + '</span></div>';
-	code += '<div class="col-fn-2 border_once scoreTotalHeader">小計</div>';
-	code += '</div>';
-
-	code += '<div class="rows scoreArea">';
-	code += '<div class="col-fn-2 border_once number">' + match.number + '</div>';
-	code += '<div class="col-fn-8">';
-	code += '<div class="rows border score">';
-
-	for(var i = 1; i <= 6; i++) {
-    var scData = score["updatedScore_" + i];
-
-    // 得点が修正済み
-    if(scData != null && scData != '') {
-      code += '<div class="col-fn-2 updated through score' + i + '" data-score="' + i + '" data-perend="' + score.perEnd + '">' + score['score_' + i] + '</div>';
-    } 
-    else {
-      code += '<div class="col-fn-2 score' + i + '" data-score="' + i + '" data-perend="' + score.perEnd + '">' + score['score_' + i] + '</div>';
-    } 
-	}
-
-	code += '</div>';
-	code += '</div>';
-	code += '<div class="col-fn-2 border_once scoreTotal">' + score.subTotal + '</div>';
-	code += '</div>';
-
-	code += '<div class="rows updatedScoreArea bottom">';
-	code += '<div class="col-fn-2 border_once number">&nbsp;</div>';
-	code += '<div class="col-fn-8">';
-	code += '<div class="rows border updatedScore">';
-
-	for(var i = 1; i <= 6; i++) {
-		var scData = score["updatedScore_" + i];
-
-		if(scData == null || scData == '') {
-			scData = '';
-      code += '<div class="col-fn-2 updatedScore' + i + '">' + scData + '</div>';
-		}
-    else {
-      code += '<div class="col-fn-2 updated updatedScore' + i + '">' + scData + '</div>';
-    }
-	}
-
-	code += '</div>';
-	code += '</div>';
-	code += '<div class="col-fn-2 border_once updatedScoreTotal">&nbsp;</div>';
-	code += '</div>';
-	code += '</div>';
-
-	$('.scoreCardBody').prepend(code);
-}
-
-function viewUpdate(perEnd, arrows, score) {
-    $('.perEnd' + perEnd + ' .updatedScore' + arrows).text(score);
-
-    $('.perEnd' + perEnd + ' .score' + arrows).addClass('updated');
-    $('.perEnd' + perEnd + ' .score' + arrows).addClass('updated through');
-    $('.perEnd' + perEnd + ' .updatedScore' + arrows).addClass('updated');
-
-    processing(perEnd);
-}
 
 function editMode(data) {
 
