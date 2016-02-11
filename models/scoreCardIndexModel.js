98567e00efe50c46440bad0e7d612f15b427cd29
TaikiFnit <taikifnitspg@gmail.com>
2016-02-11 13:34:09 +0900

    extractAvgRankingIndex 実装完了 *need debug

--- models/scoreCardIndexModel.js
+++ models/scoreCardIndexModel.js
@@ -374,11 +374,142 @@ function scoreCardIndexModel(io, connection, sessions, ios) {
 			return obj;	
 		}
 
+		function arrayAvgSort(obj) {
+
+			obj.sort(function(a, b) {
+				return a.scoreAvg < b.scoreAvg ? 1 : -1;
+			});
+
+			return obj;	
+		}
+
 		socket.on('extractAvgRankingIndex', function(data) {
 
-			// 試合に参加
-			socket.join('scoreCardIndexRoom' + data.m_id);
+			// On log
+			console.log('on extractAvgRankingIndex');
+			console.log(data);
 
+			/* Get p_id related SessionID */
+			sessions.get(addPrefix(data.sessionID), function(err, body) {
+				if(!err) {
+ 
+					var p_id = body.sess.p_id;
+					var o_id = body.sess.o_id;
+
+					if(p_id !== undefined) {
+
+						// 試合に参加
+						socket.join('scoreCardIndexRoom' + data.m_id);
+
+						// 送られてきたm_idに所属している得点表データを抽出 
+						avgRankingSql = 'select scorePerEnd.sc_id, scorePerEnd.p_id, concat(account.lastName, account.firstName) as playerName, count(scorePerEnd.sc_id) as totalPerEnd, scoreTotal.total, `match`.arrows, `match`.m_id from scorePerEnd, `match`, account, scoreCard, scoreTotal where `match`.m_id = 8 and scoreCard.m_id = 8 and scoreCard.sc_id = scorePerEnd.sc_id and scoreCard.sc_id = scoreTotal.sc_id and account.p_id = scorePerEnd.p_id group by scorePerEnd.sc_id';
+
+						// 得点表データを抽出
+						connection.query(avgRankingSql, function(avgRankingErr, avgRankingData) {
+
+							if(!avgRankingErr) {
+
+								// 抽出したデータ
+								console.log('avgRankingData');
+								console.log(avgRankingData);
+
+								var indexedP_id = {};
+
+								for(var i = 0; i < avgRankingData.length; i++) {
+
+									var p_id = avgRankingData[i]['p_id'];
+
+									// すでにp_idがkeyとして存在する
+									if(p_id in indexedP_id) {
+										// 存在する ∴ breakdownへのデータの追加とtotal, avgの更新のみを行う
+
+										indexedP_id[p_id]['scoreTotal'] += avgRankingData[i]['total'];
+										indexedP_id[p_id]['arrowsTotal'] += avgRankingData[i]['totalPerEnd'] * avgRankingData[i]['arrows'];
+									    indexedP_id[p_id]['scoreAvg'] =  indexedP_id[p_id]['scoreTotal'] / indexedP_id[p_id]['arrowsTotal'];
+									    indexedP_id[p_id]['breakdown'].push({
+											'sc_id': avgRankingData[i]['sc_id'],
+											'total': avgRankingData[i]['total']
+									    }); 
+									}
+									else {
+										// 存在しない ∴ responesのformatにそってデータをsetする
+										indexedP_id[p_id] = {};
+
+										indexedP_id[p_id]['p_id'] = avgRankingData[i]['p_id'];
+										indexedP_id[p_id]['playerName'] = avgRankingData[i]['playerName'];
+										indexedP_id[p_id]['scoreTotal'] = avgRankingData[i]['total'];
+										indexedP_id[p_id]['arrowsTotal'] = avgRankingData[i]['totalPerEnd'] * avgRankingData[i]['arrows'];
+									    indexedP_id[p_id]['scoreAvg'] =  indexedP_id[p_id]['scoreTotal'] / indexedP_id[p_id]['arrowsTotal'];
+										indexedP_id[p_id]['breakdown'] = [{
+											'sc_id': avgRankingData[i]['sc_id'],
+											'total': avgRankingData[i]['total']
+										}];
+									}
+								}
+
+								console.log('indexedP_id');
+								console.log(indexedP_id);
+
+								//socket.emit('extractAvgRankingIndex', indexedP_id);
+
+								//--- quote from extractTotalRankingIndex method ---//
+
+								var beforeSort = [];
+								var i = 0;
+
+								// 抽出したデータを配列化 (sort用)
+								Object.keys(indexedP_id).forEach(function(key) {
+									beforeSort[i] = indexedP_id[key];
+									i++;
+								});
+
+								// 配列化されたデータをtotalで昇順に並べ替える	
+								var afterSort = arrayAvgSort(beforeSort);
+
+								var rank = 1;
+
+								// 昇順に並べ替えられたarrayを利用して, response用に用意しているobjectへrankをつける
+								for(var j = 0; j < afterSort.length; j++, rank++) {
+
+									if(j != 0) {
+										// 得点重複時の処理
+										if(indexedP_id[afterSort[j].p_id].scoreAvg == indexedP_id[afterSort[j - 1].p_id].scoreAvg) {
+											indexedP_id[afterSort[j].p_id].rank = indexedP_id[afterSort[j - 1].p_id].rank; 
+											continue;
+										}
+									}
+
+									indexedP_id[afterSort[j].p_id].rank = rank; }
+
+								//  true response
+								var arrayResponseData = [];
+
+								i = 0;
+
+								// responseとして送るformatへ整形( obj to array)
+								Object.keys(indexedP_id).forEach(function(key) {
+									arrayResponseData[i] = indexedP_id[key];
+									i++;
+								});
+
+								console.log('data for extractAvgRankingIndex');
+								console.log(arrayResponseData);
+
+								socket.emit('extractAvgRankingIndex', arrayResponseData);	
+							}
+						});
+					}
+					// p_idを取得できていない = ログインができていない ∴ ログイン画面に遷移する
+					else {
+						console.log('authorizationError');
+						socket.emit('authorizationError');
+					}
+				}else {
+					console.log('faild to getSession');
+				}
+			});
+
+			/*
 			var responseData = [
 			{
 				'rank': 1,
@@ -454,6 +585,8 @@ function scoreCardIndexModel(io, connection, sessions, ios) {
 			}];
 
 			socket.emit('extractAvgRankingIndex', responseData);
+
+			*/
 		});	
 
 		socket.on('testBroadcastInsertScore', function(data) {
